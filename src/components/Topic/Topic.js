bf0919059bb0a046613fd536db0dc0cde8dd5b89
sabbir hossain <mdsabbir477470@gmail.com>
2022-10-11 20:42:56 +0600

    3 compo done

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -15,7 +15,7 @@ const Topic = ({ topic }) => {
                     <div className="card-actions justify-between items-center">
                         <h3>{ name}</h3>
                         <h3>{ total}</h3>
-                        <button className="btn btn-primary">Practice  <Link to={`/topic/${id}`}>{id}</Link><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                        <button className="btn btn-primary">Go<Link to={`/topic/${id}`}>{id}</Link><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                         </svg>
                         </button>
