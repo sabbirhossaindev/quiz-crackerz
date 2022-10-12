7c490ed9808f9210bd40640c534cbe88357b4c97
sabbir hossain <mdsabbir477470@gmail.com>
2022-10-12 17:41:41 +0600

    all work done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import './Header.css';
 
+    // This styling will be applied to a <NavLink> when the
+    // route that it links to is currently selected.
+    let activeStyle = {
+      textDecoration: "underline",
+    };
+  
+    let activeClassName = "underline";
+
 const Header = () => {
+    
     return (
         <div className="navbar bg-base-100 shadow-lg mb-8">
         <div className="flex-1">
             <h2 className='text-2xl text-rose-600'>Brain Test</h2>
         </div>
         <div className="link-list">
-            <Link to='/'>Topics</Link>
-            <Link to='/statistics'>Statistics</Link>
-            <Link to='/blog'>Blog</Link>
-        </div>
+            <NavLink to='/' style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>Topics</NavLink>
+            <NavLink to='/statistics' style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>Statistics</NavLink>
+            <NavLink to='/blog' style={({ isActive }) =>
+              isActive ? activeStyle : undefined
+            }>Blog</NavLink>
+        </div>    
     </div>
     );
 };
